900fb1bccbeead594d32ddb199ab3daf53ac885b
slapab <sla.pab.dev@gmail.com>
2015-10-28 00:57:14 +0100

    Testing on application layout. Added simple ListView into Fragment.

--- app/src/main/java/scott/mymaterialdesign/OneFragment.java
+++ app/src/main/java/scott/mymaterialdesign/OneFragment.java
@@ -7,6 +7,11 @@ import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 //import info.androidhive.materialtabs.R;
 
@@ -14,6 +19,10 @@ import android.view.ViewGroup;
 public class OneFragment extends Fragment{
 
     private SwitchCompat powerBluetoothSw ;
+
+    private ListView pairedList ;
+
+    private final String[] values = new String[] { "Device 1", "Device 2" } ;
 
     public OneFragment() {
         // Required empty public constructor
@@ -23,13 +32,31 @@ public class OneFragment extends Fragment{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState) ;
 
+
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+
+        View frag_view = inflater.inflate(R.layout.fragment_one, container, false) ;
+
+
+        pairedList = (ListView)frag_view.findViewById(R.id.paired_list) ;
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>( getActivity().getApplicationContext(),
+                R.layout.list_layout,
+                R.id.id_list_row_name,
+                values
+        );
+
+        pairedList.setAdapter(listAdapter);
+
+
+        return frag_view ;
     }
 
 }
